a73290437c54a6ab4e1b3f99db63c2a848c76bc0
WiwyP <112854329+WiwyP@users.noreply.github.com>
2025-06-03 17:28:25 +0100

    更新了 config.py 文件

---
+++ .cloudbuild/graal-build-script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+gu install native-image
+./mvnw clean install --batch-mode --quiet
+./mvnw verify --activate-profiles native \
+    --threads 3 \
+    --batch-mode \
+    --quiet \
+    --projects \!com.example:cloud-functions-sample \
+    --file native-image-samples
